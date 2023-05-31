373b7cbc495fccd9bd5017ed7e5da2d4b6034eae
trungvt138 <trungvt138@gmail.com>
2023-05-31 17:39:39 +0200

    build success but still using buffer

--- src/client.c
+++ src/client.c
@@ -1,147 +1,90 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <netdb.h>
 
-#define MAX_BUFFER_SIZE 4096
-#define EOT_CHAR 0x04
-
-void sendCommand(int socket, const char* command) {
-    // Send the command followed by the EOT character
-    dprintf(socket, "%s%c", command, EOT_CHAR);
-}
-
-void receiveResponse(int socket) {
-    char buffer[MAX_BUFFER_SIZE];
-    ssize_t bytesRead;
-
-    // Read data from the socket until EOT character is received
-    while ((bytesRead = read(socket, buffer, sizeof(buffer))) > 0) {
-        // Search for the EOT character in the received data
-        char* eotPos = memchr(buffer, EOT_CHAR, bytesRead);
-        if (eotPos != NULL) {
-            // Calculate the number of bytes until the EOT character
-            //ssize_t responseLength = eotPos - buffer;
-            
-            // Print the response
-            if (write(STDOUT_FILENO, buffer, bytesRead) < 0) {
-                perror("write");
-                return;
-            }
-            break;
-        }
-        
-        // Print the received data
-        if (write(STDOUT_FILENO, buffer, bytesRead) < 0) {
-            perror("write");
-            return;
-        }
-    }
-}
-
-void receiveFile(int socket, const char* filename) {
-    int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (file < 0) {
-        perror("File open");
-        return;
-    }
-
-    char buffer[MAX_BUFFER_SIZE];
-    ssize_t bytesRead;
-
-    // Read data from the socket until EOT character is received
-    while ((bytesRead = read(socket, buffer, sizeof(buffer))) > 0) {
-        // Search for the EOT character in the received data
-        char* eotPos = memchr(buffer, EOT_CHAR, bytesRead);
-        if (eotPos != NULL) {
-            // Calculate the number of bytes until the EOT character
-            //ssize_t fileDataLength = eotPos - buffer;
-            
-            // Write the file data to the file
-            if (write(file, buffer, bytesRead) < 0) {
-                perror("write");
-                return;
-            }
-            break;
-        }
-        
-        // Write the received data to the file
-        if (write(file, buffer, bytesRead) < 0) {
-            perror("write");
-            return;
-        }
-
-    }
-
-    close(file);
-}
+#define MAX_COMMAND_LENGTH 256
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        printf("Usage: %s <server IP> <server port>\n", argv[0]);
-        return 1;
+  if (argc != 3) {
+    printf("Usage: %s <server_address> <server_port>\n", argv[0]);
+    return 1;
+  }
+
+  const char* serverAddress = argv[1];
+  const char* serverPort = argv[2];
+
+  struct addrinfo hints, *serverInfo;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo(serverAddress, serverPort, &hints, &serverInfo) != 0) {
+    perror("getaddrinfo");
+    return 1;
+  }
+
+  int clientSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+  if (clientSocket < 0) {
+    perror("Socket");
+    return 1;
+  }
+
+  if (connect(clientSocket, serverInfo->ai_addr, serverInfo->ai_addrlen) < 0) {
+    perror("Connect");
+    return 1;
+  }
+
+  printf("Connected to server. Enter commands (List, Files, Get <filename>, Put <filename>, Quit):\n");
+
+  char command[MAX_COMMAND_LENGTH];
+
+  while (1) {
+    printf("> ");
+    fflush(stdout);
+
+    if (fgets(command, sizeof(command), stdin) == NULL) {
+      break;
     }
 
-    const char* serverIP = argv[1];
-    int serverPort = atoi(argv[2]);
+    // Remove trailing newline character
+    command[strcspn(command, "\n")] = '\0';
 
-    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketfd < 0) {
-        perror("Socket");
-        return 1;
+    if (send(clientSocket, command, strlen(command), 0) < 0) {
+      perror("Send");
+      break;
     }
 
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(serverPort);
-    if (inet_pton(AF_INET, serverIP, &(serverAddr.sin_addr)) <= 0) {
-        perror("inet_pton");
-        return 1;
+    if (strcmp(command, "Quit") == 0) {
+      printf("Exiting...\n");
+      break;
     }
 
-    if (connect(socketfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Connect");
-        return 1;
+    // Receive and display the server response
+    char response[MAX_COMMAND_LENGTH];
+    ssize_t bytesRead;
+
+    if ((bytesRead = recv(clientSocket, response, sizeof(response) - 1, 0)) < 0) {
+      perror("Receive");
+      break;
     }
 
-    printf("Connected to server\n");
-
-    // Receive and print the welcome message
-    receiveResponse(socketfd);
-
-    char command[256];
-    while (1) {
-        printf("Enter command: ");
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            perror("fgets");
-            return 1;
-        }
-
-        // Remove the trailing newline character
-        command[strcspn(command, "\n")] = '\0';
-
-        if (strcmp(command, "exit") == 0) {
-            // Send the exit command and break out of the loop
-            sendCommand(socketfd, command);
-            break;
-        }
-
-        // Send the command to the server
-        sendCommand(socketfd, command);
-
-        // Check the command type and receive the appropriate response
-        if (strncmp(command, "Get ", 4) == 0) {
-            receiveFile(socketfd, command + 4);
-        } else {
-            receiveResponse(socketfd);
-        }
+    if (bytesRead == 0) {
+      printf("Server closed the connection\n");
+      break;
     }
 
-    close(socketfd);
-    printf("Disconnected from server\n");
+    response[bytesRead] = '\0';
+    printf("Response: %s\n", response);
+  }
 
-    return 0;
+  freeaddrinfo(serverInfo);
+  close(clientSocket);
+  return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,5 +1,4 @@
 #include <arpa/inet.h>
-#include <dirent.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,160 +7,246 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/select.h>
+#include <dirent.h>
 #include <sys/stat.h>
 #include <time.h>
-#include <fcntl.h>
+#include <netdb.h>
 
 #define DEFAULT_PORT 0
 #define MAX_CLIENTS 10
-#define BUFFER_SIZE 1024
-const char EOT_CHAR = 4;
 
 typedef struct {
   char hostname[256];
   int port;
 } ClientInfo;
 
-void sendStream(int clientSocket, const char* data, ssize_t dataSize) {
-  ssize_t bytesSent;
-  ssize_t totalSent = 0;
+void handleListCommand(int clientSocket, int* clientSockets) {
+  // Create an array to store the client information
+  ClientInfo clients[MAX_CLIENTS];
+  int numClients = 0;
 
-  while (totalSent < dataSize) {
-    bytesSent = send(clientSocket, data + totalSent, dataSize - totalSent, 0);
-    if (bytesSent < 0) {
-      perror("Send");
-      return;
+  // Loop through all connected sockets and get client information
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (clientSockets[i] != -1) {
+      struct sockaddr_in clientAddr;
+      unsigned int addrLen = sizeof(clientAddr);
+      getpeername(clientSockets[i], (struct sockaddr*)&clientAddr, &addrLen);
+
+      // Store client hostname and port in the array
+      strcpy(clients[numClients].hostname, inet_ntoa(clientAddr.sin_addr));
+      clients[numClients].port = ntohs(clientAddr.sin_port);
+      numClients++;
     }
-    totalSent += bytesSent;
   }
+
+  // Create the response string
+  char response[256];
+  sprintf(response, "Connected Clients:\n");
+  for (int i = 0; i < numClients; i++) {
+    char clientInfo[256];
+    sprintf(clientInfo, "%s:%d\n", clients[i].hostname, clients[i].port);
+    strcat(response, clientInfo);
+  }
+  sprintf(response + strlen(response), "Total Clients: %d\n", numClients);
+
+  // Send the response back to the client
+  send(clientSocket, response, strlen(response), 0);
 }
 
-void handleCommand(int clientSocket, int* clientSockets, const char* command) {
-  if (strcmp(command, "List") == 0) {
-    // Create an array to store the client information
-    ClientInfo clients[MAX_CLIENTS];
-    int numClients = 0;
+void handleFilesCommand(int clientSocket) {
+  // Open the server directory
+  DIR* dir;
+  struct dirent* entry;
+  struct stat fileStat;
+  char fileAttributes[256];
+  int numFiles = 0;
 
-    // Loop through all connected sockets and get client information
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-      if (clientSockets[i] != -1) {
-        struct sockaddr_in clientAddr;
-        unsigned int addrLen = sizeof(clientAddr);
-        getpeername(clientSockets[i], (struct sockaddr*)&clientAddr, &addrLen);
-
-        // Store client hostname and port in the array
-        strcpy(clients[numClients].hostname, inet_ntoa(clientAddr.sin_addr));
-        clients[numClients].port = ntohs(clientAddr.sin_port);
-        numClients++;
-      }
-    }
-
-    // Create the response string
-    char response[256];
-    sprintf(response, "Connected Clients:\n");
-    for (int i = 0; i < numClients; i++) {
-      char clientInfo[256];
-      sprintf(clientInfo, "%s:%d\n", clients[i].hostname, clients[i].port);
-      strcat(response, clientInfo);
-    }
-    sprintf(response + strlen(response), "Total Clients: %d\n", numClients);
-
-    // Send the response back to the client as a stream
-    sendStream(clientSocket, response, strlen(response));
-    sendStream(clientSocket, "\n", 1);
-    sendStream(clientSocket, &EOT_CHAR, 1);
+  dir = opendir(".");
+  if (dir == NULL) {
+    perror("opendir");
+    return;
   }
-  else if (strcmp(command, "Files") == 0) {
-    // Files command implementation
 
-    // Open the server directory
-    DIR* dir;
-    struct dirent* entry;
-    struct stat fileStat;
-    char fileAttributes[256];
-    int numFiles = 0;
+  // Prepare the response string
+  char response[4096] = "List of Files:\n";
 
-    dir = opendir(".");
-    if (dir == NULL) {
-      perror("opendir");
-      return;
-    }
-
-    // Prepare the response string
-    char response[4096] = "List of Files:\n";
-
-    // Read directory entries and get file information
-    while ((entry = readdir(dir)) != NULL) {
-      if (stat(entry->d_name, &fileStat) < 0) {
-        perror("stat");
-        continue;
-      }
-
-      // Format the file attributes
-      strftime(fileAttributes, sizeof(fileAttributes), "%Y-%m-%d %H:%M:%S", localtime(&fileStat.st_mtime));
-
-      // Append the file information to the response string
-      char fileInfo[1024];
-      sprintf(fileInfo, "%s - Size: %ld bytes, Last Modified: %s\n", entry->d_name, fileStat.st_size, fileAttributes);
-      strcat(response, fileInfo);
-      numFiles++;
-    }
-
-    // Close the directory
-    closedir(dir);
-
-    // Append the total number of files to the response string
-    sprintf(response + strlen(response), "Total Files: %d\n", numFiles);
-
-    // Send the response back to the client as a stream
-    sendStream(clientSocket, response, strlen(response));
-    sendStream(clientSocket, "\n", 1);
-    sendStream(clientSocket, &EOT_CHAR, 1);
-  }
-  else if (strncmp(command, "Get", 3) == 0) {
-    // Get command implementation
-    char fileName[256];
-    char fileAttributes[256];
-
-    // Extract the file name from the command
-    sscanf(command, "Get %s", fileName);
-
-    // Open the file in binary mode for reading
-    FILE* file = fopen(fileName, "rb");
-    if (file == NULL) {
-      perror("fopen");
-      sendStream(clientSocket, "File not found.\n", 16);
-      sendStream(clientSocket, (char*)&EOT_CHAR, 1);
-      return;
-    }
-
-    // Get file size and last modified time
-    struct stat fileStat;
-    if (stat(fileName, &fileStat) < 0) {
+  // Read directory entries and get file information
+  while ((entry = readdir(dir)) != NULL) {
+    if (stat(entry->d_name, &fileStat) < 0) {
       perror("stat");
-      fclose(file);
-      return;
+      continue;
     }
 
     // Format the file attributes
     strftime(fileAttributes, sizeof(fileAttributes), "%Y-%m-%d %H:%M:%S", localtime(&fileStat.st_mtime));
 
-    // Send the file attributes as a stream
-    sendStream(clientSocket, fileAttributes, strlen(fileAttributes));
-    sendStream(clientSocket, "\n", 1);
+    // Append the filename and attributes to the response string
+    snprintf(response + strlen(response), sizeof(response) - strlen(response), "%s\t%s\n", entry->d_name, fileAttributes);
 
-    // Send the file contents as a stream
-    char buffer[BUFFER_SIZE];
-    ssize_t bytesRead;
+    numFiles++;
+  }
 
-    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-      sendStream(clientSocket, buffer, bytesRead);
-    }
+  // Append the total number of files to the response string
+  snprintf(response + strlen(response), sizeof(response) - strlen(response), "Total Files: %d\n", numFiles);
 
+  closedir(dir);
+
+  // Send the response to the client
+  if (send(clientSocket, response, strlen(response), 0) < 0) {
+    perror("Send");
+    return;
+  }
+}
+
+void handleGetCommand(int clientSocket, const char* command) {
+  // Parse the command to extract the filename and file attributes
+  char filename[256];
+  char fileAttributes[256];
+  sscanf(command, "Get %s %[^\n]", filename, fileAttributes);
+
+  // Open the file for reading
+  FILE* file = fopen(filename, "r");
+  if (file == NULL) {
+    perror("File open");
+    return;
+  }
+
+  // Read the file content and calculate the file size
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  // Read the file content into a buffer
+  char* fileContent = (char*)malloc(fileSize + 1);
+  if (fileContent == NULL) {
+    perror("Memory allocation");
     fclose(file);
+    return;
+  }
 
-    // Send the EOT character to mark the end of the stream
-    sendStream(clientSocket, &EOT_CHAR, 1);
+  size_t bytesRead = fread(fileContent, 1, fileSize, file);
+  if (bytesRead != (size_t)fileSize) {
+      perror("fread");
+      return;// Handle the error or exit the function
+  }
+
+  fileContent[fileSize] = '\0';
+
+  fclose(file);
+
+  // Get the last modified time of the file
+  struct stat fileStat;
+  if (stat(filename, &fileStat) == -1) {
+    perror("File stat");
+    free(fileContent);
+    return;
+  }
+  time_t lastModified = fileStat.st_mtime;
+
+  // Prepare the response with the file content and attributes
+  char response[4096];
+  snprintf(response, sizeof(response), "Filename: %s\nLast Modified: %s\nSize: %ld bytes\n\n%s",
+           filename, ctime(&lastModified), fileSize, fileContent);
+
+  free(fileContent);
+
+  // Send the response to the client
+  if (send(clientSocket, response, strlen(response), 0) < 0) {
+    perror("Send");
+    return;
+  }
+}
+
+void handlePutCommand(int clientSocket, const char* command) {
+  // Extract the filename from the command
+  const char* filename = command + 4;
+
+  // Create a new file in the server directory
+  FILE* file = fopen(filename, "w");
+  if (file == NULL) {
+    perror("File open");
+    return;
+  }
+
+  // Set up the file descriptor set for select
+  fd_set read_fds;
+  FD_ZERO(&read_fds);
+  FD_SET(clientSocket, &read_fds);
+
+  // Set the timeout for select to 1 second
+  struct timeval timeout;
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
+
+  // Receive and store the file data
+  char buffer[1024];
+  ssize_t bytesRead;
+  while (select(clientSocket + 1, &read_fds, NULL, NULL, &timeout) > 0) {
+    if ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+      fwrite(buffer, 1, bytesRead, file);
+    }
+  }
+
+  fclose(file);
+
+  // Get the server hostname and IP address
+  char hostname[256];
+  if (gethostname(hostname, sizeof(hostname)) < 0) {
+    perror("gethostname");
+    return;
+  }
+
+  struct addrinfo hints, *serverInfo;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo(hostname, NULL, &hints, &serverInfo) != 0) {
+    perror("getaddrinfo");
+    return;
+  }
+
+  char serverIP[INET_ADDRSTRLEN];
+  if (inet_ntop(AF_INET, &((struct sockaddr_in*)serverInfo->ai_addr)->sin_addr, serverIP, sizeof(serverIP)) == NULL) {
+    perror("inet_ntop");
+    return;
+  }
+
+  freeaddrinfo(serverInfo);
+
+  // Get the current date and time
+  time_t rawTime;
+  struct tm* timeInfo;
+  time(&rawTime);
+  timeInfo = localtime(&rawTime);
+  if (timeInfo == NULL) {
+    perror("localtime");
+    return;
+  }
+  char datetime[64];
+  strftime(datetime, sizeof(datetime), "%Y-%m-%d %H:%M:%S", timeInfo);
+
+  // Send the response to the client
+  char response[512];
+  snprintf(response, sizeof(response), "OK %s\n%s\n%s", hostname, serverIP, datetime);
+  if (send(clientSocket, response, strlen(response), 0) < 0) {
+    perror("Send");
+    return;
+  }
+}
+
+void handleCommand(int clientSocket, int* clientSockets, const char* command) {
+  if (strcmp(command, "List") == 0) {
+    handleListCommand(clientSocket, clientSockets);
+  }
+  else if (strcmp(command, "Files") == 0) {
+    handleFilesCommand(clientSocket);
+  }
+  else if (strncmp(command, "Get", 3) == 0) {
+    handleGetCommand(clientSocket, command);
+  }
+  else if (strncmp(command, "Put", 3) == 0) {
+    handlePutCommand(clientSocket, command);
   }
 }
 
@@ -230,24 +315,24 @@ int main(int argc, char** argv) {
     read_fds = master;
 
     if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
-        perror("Select");
-        return 1;
+      perror("Select");
+      return 1;
     }
 
     if (FD_ISSET(s_tcp, &read_fds)) {
       int newSocket;
       if ((newSocket = accept(s_tcp, (struct sockaddr*)&sa_client, &sa_len)) < 0) {
-          perror("Accept");
-          close(s_tcp);
-          return 1;
+        perror("Accept");
+        close(s_tcp);
+        return 1;
       }
 
       // Add the new client socket to the array
       int i;
       for (i = 0; i < MAX_CLIENTS; i++) {
         if (clientSockets[i] == -1) {
-            clientSockets[i] = newSocket;
-            break;
+          clientSockets[i] = newSocket;
+          break;
         }
       }
 
@@ -257,7 +342,6 @@ int main(int argc, char** argv) {
       }
 
       printf("New connection established\n");
-      printf("%d\n", fdmax);
     }
 
     for (int i = 0; i < MAX_CLIENTS; i++) {
